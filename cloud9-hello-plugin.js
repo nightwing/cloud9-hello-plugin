01ce9f37c701b536f31070316931e5d45f4a031b
nightwing <amirjanyan@gmail.com>
2013-09-24 18:43:20 +0400

    test

--- cloud9-hello-plugin.js
+++ cloud9-hello-plugin.js
@@ -1,7 +1,18 @@
 define(function(require, exports, module) {
 
 var ext = require("core/ext");
-var helloDep = require("./dependency.js");
+var multiselect = require("ace/multi_select")
+var Editor = require("ace/editor").Editor
+
+var Search = require("ace/search").Search;
+var search = new Search();
+
+function find(session, needle, dir) {
+    search.$options.wrap = true;
+    search.$options.needle = needle;
+    search.$options.backwards = dir == -1;
+    return search.find(session);
+}
 
 module.exports = ext.register("ext/helloplugin/helloplugin", {
     name    : "cloud9-hello-plugin",
@@ -12,7 +23,27 @@ module.exports = ext.register("ext/helloplugin/helloplugin", {
     alone   : true,
 
     hook : function() {
-        console.log(helloDep.getHello());
+        Editor.prototype.selectMore = function (dir, skip) {
+            var session = this.session;
+            var sel = session.multiSelect;
+
+            var range = sel.toOrientedRange();
+            if (range.isEmpty()) {
+                var range = session.getWordRange(range.start.row, range.start.column);
+                range.cursor = range.end;
+                this.multiSelect.addRange(range);
+                return;
+            }
+            var needle = session.getTextRange(range);
+
+            var newRange = find(session, needle, dir);
+            if (newRange) {
+                newRange.cursor = dir == -1 ? newRange.start : newRange.end;
+                this.multiSelect.addRange(newRange);
+            }
+            if (skip)
+                this.multiSelect.substractPoint(range.cursor);
+        }
     },
 
     enable: function () {},
